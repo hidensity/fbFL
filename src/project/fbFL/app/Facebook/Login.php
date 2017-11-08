0ac9b83a90f28c3810d4f70161536fff0d4b0b32
dennis <hidensity@gmx.net>
2017-11-08 22:06:11 +0100

    Initial commit

---
+++ src/project/fbFL/app/Facebook/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dbb\Facebook;
+
+use Facebook\Facebook;
+
+class Login
+{
+    /** @var Facebook */
+    protected $facebook;
+
+    /**
+     * Login constructor.
+     */
+    public function __construct()
+    {
+        echo 'Foo! Bar! Baz!';
+    }
+}
---
+++ src/project/fbFL/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+define('PROJECT_BASE_DIR', realpath('../'));
+define('SRC_ROOT_DIR', realpath(PROJECT_BASE_DIR . DIRECTORY_SEPARATOR . '../../'));
+set_include_path(get_include_path() . PATH_SEPARATOR . PROJECT_BASE_DIR . PATH_SEPARATOR . SRC_ROOT_DIR);
+
+use Dbb\Facebook\Login;
+
+try {
+    require '../../../vendor/autoload.php';
+
+    $login = new Login();
+} catch (\Exception $e) {
+    // For debugging
+    ini_set('display_errors', 1);
+    throw $e;
+}
